2a065e12bed293af0ae9bdfc2025e1d06a09967a
7heMagician <obbengvincent@gmail.com>
2024-03-01 18:28:18 +0000

    Revert "Merge pull request #18 from Vi-obb/og-image"
    This reverts commit 412defd9b24f102b0fbe6bc91ef2e5b4b40a9aab, reversing
    changes made to aee673122fd7a66d2df6b799373ac6ec4c309472.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
 
-import vercel from '@astrojs/vercel/serverless';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.vincentobbeng.com',
+  site: "https://vincentobbeng.com",
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
   ],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
 });
